8b004a0d082b62d7eb12c16fdce314bc4bb26524
bchen2890 <99276640+bchen2890@users.noreply.github.com>
2022-06-01 19:12:08 +0200

    Alphabets and numbers

---
+++ patterns.js
@@ -0,0 +1,8 @@
+export const patterns = {
+  naturalNumbers: /^\d+$/,
+  wholeNumbers: /^-?\d+$/,
+  positiveDecimalNumbers: /^\d*(\.\d+)?$/,
+  decimalNumbers: /^-?\d*(\.\d+)?$/,
+  alphabets:/^[a-zA-Z]*$/,
+  alphanumeric: /^[a-zA-Z0-9]*$/
+}
