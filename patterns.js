1360ad73296417adda3ed7caf2ad41a5eb202fd6
bchen2890 <99276640+bchen2890@users.noreply.github.com>
2022-06-02 20:30:27 +0200

    Email, date and time

--- patterns.js
+++ patterns.js
@@ -4,5 +4,9 @@ export const patterns = {
   positiveDecimalNumbers: /^\d*(\.\d+)?$/,
   decimalNumbers: /^-?\d*(\.\d+)?$/,
   alphabets:/^[a-zA-Z]*$/,
-  alphanumeric: /^[a-zA-Z0-9]*$/
+  alphanumeric: /^[a-zA-Z0-9]*$/,
+  email:/^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6})*$/,
+  dateYYYY_MM_dd:/([12]\d{3})[\/\-](0[1-9]|1[0-2])[\/\-](0[1-9]|[12]\d|3[01])/,
+  datedd_MM_YYYY:/^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/,
+  time_HH_MM_SS:/(?:[01]\d|2[0123]):(?:[012345]\d):(?:[012345]\d)/
 }
